870bb1e2205500c27d89a9ff7eb40ff7c568172b
viniduminsara <viniduminsara@gmail.com>
2023-12-29 13:11:52 +0530

    few changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,21 @@
 import {country_list} from './country_list.js';
 
 const droplist = $('form select');
-const fromCurrency = $('.drop-list .from select')[0];
-const toCurrency = $('.drop-list .to select')[0];
-const formButton = $('form button')[0];
-
-console.log(droplist);
-console.log(fromCurrency);
-console.log(toCurrency);
-console.log(formButton);
+const fromCurrency = $('.drop-list .from select').eq(0);
+const toCurrency = $('.drop-list .to select').eq(0);
+const formButton = $('form button').eq(0);
+const amount = $('form input').eq(0);
+const fromRate = $('.rates .from').eq(0);
+const toRate = $('.rates .to').eq(0);
 
 for(let i = 0; i < droplist.length; i++){
 
     for(let currency_code in country_list){
+
         let selected = i == 0 ? currency_code == "USD" ? "selected" : "" : currency_code == "LKR" ? "selected" : "";
-
         let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
-
         droplist[i].insertAdjacentHTML('beforeend', optionTag);
+
     }
 
     droplist.eq(i).on('change', (e) => {
@@ -25,6 +23,40 @@ for(let i = 0; i < droplist.length; i++){
     });
 }
 
+$(document).ready(function () {
+    getExchangeRates();
+});
+
+formButton.on('click', (e) => {
+    e.preventDefault();
+    getExchangeRates();
+})
+
 function loadflag(element){
-    console.log(element.value);
+    for(let country in country_list){
+        if(country === element.value){
+            let imgTag = $(element).parent().find("img")[0];
+            imgTag.src = `https://flagcdn.com/12x12/${country_list[country].toLowerCase()}.png`;
+        }
+    }
+}
+
+function getExchangeRates(){
+    let amountVal = parseInt(amount.val()) || 1;
+    const url = `https://v6.exchangerate-api.com/v6/3759abf04bc8630a81f3a7dd/latest/${fromCurrency.val()}`;
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        success: function(result){
+            let exRate = result.conversion_rates[toCurrency.val()];
+            let totalAmount = (amountVal * exRate).toFixed(2);
+            fromRate.text(`${amountVal} ${fromCurrency.val()}`);
+            toRate.text(`${totalAmount} ${toCurrency.val()}`);
+        },
+        error: function(err){
+            alert('Something went wrong 😔');
+        }
+    })
 }
